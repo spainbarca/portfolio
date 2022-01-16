1551908b204699aa29367fb40bf989a4c2f227ed
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-16 12:28:55 -0500

    Blade, motor de plantillas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home</title>
-</head>
-<body>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/portfolio">Portfolio</a></li>
-			<li><a href="/contact">Contact</a></li>
-		</ul>
-	</nav>
-	<h1>Home</h1>
-	Bienvenid@ <?php echo $nombre ?? "Invitado" ?>
+@extends('layout')
 
-</body>
-</html>
+@section('title', 'Home')
+
+@section('content')
+	<h1>Home</h1>
+@endsection
