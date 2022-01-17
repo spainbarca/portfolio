090095660de6a0e5accdd500a85f6b5954121f8e
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-16 23:39:29 -0500

    Controladores Resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::view('/', 'home')->name('home');
+/*Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::get('/portfolio', 'PortfolioController')->name('portfolio');
-Route::view('/contact', 'contact')->name('contact');
+Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
+Route::view('/contact', 'contact')->name('contact');*/
+
+Route::resource('projects', 'PortfolioController')->except(['index', 'show']);
