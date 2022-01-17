815cbcfb1af36af8c4b823dc1c41773c2ab354a2
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-16 21:18:07 -0500

    Estructuras de control con blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,4 +4,20 @@
 
 @section('content')
 	<h1>Portfolio</h1>
+
+	@for()
+	@endfor
+
+	@while()
+	@endwhile
+
+	@switch()
+
+	<ul>
+		@forelse ($portfolio as $portfolioItem)
+			<li>{{$portfolioItem['title']}}<small>{{$loop->first ? 'Es el primero' : ''}}</small></li>
+		@empty
+			<li>No hay proyectos para mostrar</li>
+		@endforelse
+	</ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+$portfolio = [
+    ['title' => 'Proyecto #1'],
+    ['title' => 'Proyecto #2'],
+    ['title' => 'Proyecto #3'],
+    ['title' => 'Proyecto #4'],
+];
+
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
 
 // aprendible.com = Route::get('/', function)
