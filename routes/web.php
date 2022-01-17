b5e829d6b6497a15fe3f810b42c1692015e343c5
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-17 01:05:58 -0500

    Cómo enviar formularios

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ Route::view('/', 'home')->name('home');
 Route::view('/about/{param?}', 'about')->name('about');
 Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
+
+Route::post('contact', 'MessagesController@store');
