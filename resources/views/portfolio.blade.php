df32573918078e65ea01818f80d100f205102e7f
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-16 23:11:11 -0500

    Controladores (invoke)

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,15 +4,6 @@
 
 @section('content')
 	<h1>Portfolio</h1>
-
-	@for()
-	@endfor
-
-	@while()
-	@endwhile
-
-	@switch()
-
 	<ul>
 		@forelse ($portfolio as $portfolioItem)
 			<li>{{$portfolioItem['title']}}<small>{{$loop->first ? 'Es el primero' : ''}}</small></li>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,6 @@
 <?php
 
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    ['title' => 'Proyecto #2'],
-    ['title' => 'Proyecto #3'],
-    ['title' => 'Proyecto #4'],
-];
-
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio', 'PortfolioController')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
-
-// aprendible.com = Route::get('/', function)
-//aprendible.com/contacto = Route::get('contacto', function)
-
-/*Route::get('/', function(){
-    $nombre = "Noah";
-    return view('home', compact('nombre')); //['nombre' => $nombre]
-})->name('home');*/
-
-//Route::view('/', 'home')->name('/home'); //Políticas de privacidad, términos y condiciones
-
