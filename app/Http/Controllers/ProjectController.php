aa444df2bd3696eb1669ad7dea2b65947846a957
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-23 17:48:24 -0500

    Route Model Binding (rutas slug)

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -42,10 +42,11 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Project $project)
     {
+        //return $id;
         return view('projects.show', [
-            'project' => Project::findOrFail($id),
+            'project' => $project,
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ App::setlocale("es");
 Route::view('/', 'home')->name('home');
 Route::view('/about/{param?}', 'about')->name('about');
 Route::get('/portfolio', 'ProjectController@index')->name('projects.index');
-Route::get('/portfolio/{id}', 'ProjectController@show')->name('projects.show');
+Route::get('/portfolio/{project}', 'ProjectController@show')->name('projects.show');
 Route::view('/contact', 'contact')->name('contact');
 
 Route::post('contact', 'MessageController@store')->name('messages.store');
