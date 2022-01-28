1b14d5b9909ee9708dc6ab8d34b581e4e54d2363
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-27 23:56:55 -0500

    Eloquent Eliminar registros

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -75,9 +75,10 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+        return redirect()->route('projects.index');
     }
 
     public function create()
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/portfolio/{project}/editar', 'ProjectController@edit')->name('proje
 Route::patch('/portfolio/{project}', 'ProjectController@update')->name('projects.update');
 Route::post('/portfolio', 'ProjectController@store')->name('projects.store');
 Route::get('/portfolio/{project}', 'ProjectController@show')->name('projects.show');
+Route::delete('/portfolio/{project}', 'ProjectController@destroy')->name('projects.destroy');
 
 Route::view('/contact', 'contact')->name('contact');
 
