af4f807ee44382d33ca38ceb3ca8a9030b8f43da
spainbarca <spain.barcelona.1999@gmail.com>
2022-01-28 00:09:02 -0500

    Route resource

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,9 @@ App::setlocale("es");
 Route::view('/', 'home')->name('home');
 Route::view('/about/{param?}', 'about')->name('about');
 
-Route::get('/portfolio', 'ProjectController@index')->name('projects.index');
-Route::get('/portfolio/crear', 'ProjectController@create')->name('projects.create');
-Route::get('/portfolio/{project}/editar', 'ProjectController@edit')->name('projects.edit');
-Route::patch('/portfolio/{project}', 'ProjectController@update')->name('projects.update');
-Route::post('/portfolio', 'ProjectController@store')->name('projects.store');
-Route::get('/portfolio/{project}', 'ProjectController@show')->name('projects.show');
-Route::delete('/portfolio/{project}', 'ProjectController@destroy')->name('projects.destroy');
+Route::resource('portfolio', 'ProjectController')
+	->parameters(['portfolio' => 'project'])
+	->names('projects');
 
 Route::view('/contact', 'contact')->name('contact');
 
