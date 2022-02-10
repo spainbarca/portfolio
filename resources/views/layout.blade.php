27a90191558621f335180358b39540677441467b
spainbarca <spain.barcelona.1999@gmail.com>
2022-02-09 22:10:21 -0500

    Introducción a Laravel mix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,11 +2,9 @@
 <html>
 <head>
 	<title>@yield('title', 'Aprendible')</title>
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+	<script src="/js/app.js" defer></script>
 	<style>
-		.active a {
-			color: red;
-			text-decoration: none;
-		}
 	</style>
 </head>
 <body>
